86fceccdcebe3815b1413f011a4f8874235b9859
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-04-16 14:54:54 +0530

    Added MongoConnection and Session Secret

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
-
+const session = require("express-session");
+const mdb = require("./config/mongodb");
+const mongoStore = require("connect-mongo");
+const keys = require("./config/keys");
 const app = express();
+
+app.use(
+  session({
+    secret: keys.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+    store: mongoStore.create({
+      clientPromise: mdb,
+    }),
+  })
+);
 
 app.use("/", require("./routes"));
 
